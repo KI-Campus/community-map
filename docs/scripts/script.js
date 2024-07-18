ca81fe2f6453c5317a5853a121b4659cdb59f7e1
frederikho <frederik.holtel@gmx.net>
2024-07-18 11:58:25 +0200

    Add offset back in and remove display none for tooltip calls

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -265,7 +265,7 @@ function drawMap() {
     .attr("class", "tooltip")
     .on('mouseleave', function () { 
       svg.selectAll('circle').transition().duration(100).attr('r', 3.5);
-      return tooltip.style("display", "none"); 
+      // return tooltip.style("display", "none");   
     });
 
   //changing the projection from mercator to albers
@@ -340,7 +340,7 @@ function addCityLocations(cities) {
           if (window.innerWidth - d3.event.pageX < 130){
             tooltip.style("top", (d3.event.pageY + 20) + "px").style("left", (window.innerWidth - 150) + "px");
           } else {
-            tooltip.style("top", (d3.event.pageY + 0) + "px").style("left", (d3.event.pageX - 3) + "px");
+            tooltip.style("top", (d3.event.pageY + 20) + "px").style("left", (d3.event.pageX - 3) + "px");
           }
           if (d.institutions.length > 10) {
             tooltip.style("height", "35vh");
@@ -356,9 +356,9 @@ function addCityLocations(cities) {
           if(isTouchDevice()){
             d3.select(this)
               .transition()
-              .duration(100)
+              .duration(1000)
               .attr('r', 3.5);
-            return tooltip.style("display", "none");
+            // return tooltip.style("display", "none");
           }
         });
 
